4a4990cd5eb1cca59f00f49ed55b9a77e0fbb433
dislh888 <dislh888@gmail.com>
2024-11-18 13:29:08 +0800

    Delete PySpark-learn directory

--- PySpark-learn/01_构建环境.py
+++
@@ -1,8 +0,0 @@
-from pyspark import SparkConf, SparkContext
-import os
-os.environ["JAVA_HOME"] = "D:/Program Files/Java"
-# 创建类对象
-conf = SparkConf().setMaster('local[*]').setAppName('1')
-sc = SparkContext(conf=conf)
-print(sc.version)
-sc.stop()
